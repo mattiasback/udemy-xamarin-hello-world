88d815c08a4eaa9eaba069b2607796b2bdde4bf7
Mattias Bäck <mattias.back@milou.se>
2020-06-09 15:32:08 +0200

    Replace deprecated method of checking platform, with new method

--- src/HelloWorld/HelloWorld/GreetPage.xaml.cs
+++ src/HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -18,29 +18,17 @@ namespace HelloWorld
 
             GreetSlider.Value = 0;
 
-            //Explicit
-            if (Device.OS == TargetPlatform.iOS)
-            {
-                Padding = new Thickness(0, 20, 0, 0);
-            }
-            else if(Device.OS == TargetPlatform.Android)
-            {
-                Padding = new Thickness(0, 20, 0, 10);
-            }
-
-            //Generic
-            Device.OnPlatform<Thickness>(
-                iOS: new Thickness(0, 20, 0, 0),
-                Android: new Thickness(0, 20, 0, 10),
-                WinPhone: new Thickness(0, 20, 0, 0)
-            );
-
-            //Non-generic
-            Device.OnPlatform(
-                iOS: () => { Padding = new Thickness(0, 20, 0, 0); },
-                Android: () => { Padding = new Thickness(0, 20, 0, 0);}
-            );
+            switch (Device.RuntimePlatform) {
+                case Device.iOS: {
+                    Padding = new Thickness(0, 20, 0, 0);
+                    break;
+                }
+                default: {
+                    Padding = new Thickness(0, 0, 0, 0);
+                    break;
+                }
+            };
         }
-
     }
+
 }
