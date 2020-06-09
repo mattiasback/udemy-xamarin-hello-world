04c556aedad78c000199cd36a3df95844c5be66e
Mattias Bäck <mattias.back@milou.se>
2020-06-09 15:35:53 +0200

    Add global padding in XAML instead

--- src/HelloWorld/HelloWorld/GreetPage.xaml.cs
+++ src/HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -17,17 +17,6 @@ namespace HelloWorld
             InitializeComponent();
 
             GreetSlider.Value = 0;
-
-            switch (Device.RuntimePlatform) {
-                case Device.iOS: {
-                    Padding = new Thickness(0, 20, 0, 0);
-                    break;
-                }
-                default: {
-                    Padding = new Thickness(0, 5, 0, 0);
-                    break;
-                }
-            };
         }
     }
 
